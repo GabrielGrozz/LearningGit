496072357f528000b6a260a90055f249a2dc74f5
Gabriel Luis <gabrielmemes268825@gmail.com>
2022-12-10 17:53:36 -0300

    atualização

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('Hello World')
+/*
+git init - você cria um repositório na pasta 
+
+git status- você irá ver os arquivos que estão atualizados e não estão no controle de versão
+
+git add "nome do arquivo" /// ou coloque apenas um ponto para adicionar todos os arquivos - serve para adicionar um arquivo no controle de versão
+
+git commit -m "mensagem" - criamos uma versão do nosso projeto
+
+git remote add origin *link do repositório* - estaremos linkando o nosso projeto a um repositório 
+
+git push - envia nossos arquivos para a nuvem
+
+------ branchs serião divisões ou versões diferentes do seu código
+
+
+
+
+*/
